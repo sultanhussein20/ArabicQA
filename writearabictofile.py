34990e721e541c98a454dfcb27d70dfeb9a7e02e
a-mustafa <ahmadmu2011@gmail.com>
2023-08-12 23:18:05 +0300

    Add files via upload

---
+++ writearabictofile.py
@@ -0,0 +1,7 @@
+filename="D:\ArabicQA\data.txt"
+with open(filename,'w', encoding='utf-32') as file:
+    file.write("اسمي حسن")
+    file.write("\n")
+    file.write("عمري 20")
+    file.write("\n")
+    file.write("ألعب كرة القدم في نادي الحارة")
